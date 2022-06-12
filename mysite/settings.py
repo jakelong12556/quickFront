d6dca0fd1464f58816b30afb2f901d92ea092f64
Phan Minh Cuong <Jakelong12556@yahoo.com>
2022-06-12 23:17:36 +0100

    update settings.py collectstatic set new static location

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "blog/static/",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
